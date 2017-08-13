3a5b3ec52c8d616d21efb7d23a780af2f0f64b40
Tyler Haas <haasdevelopment@gmail.com>
2017-08-12 23:10:41 -0500

    pass all tests

--- test/map.test.js
+++ test/map.test.js
@@ -5,7 +5,7 @@ const sinon = require('sinon')
 const map = require('../src/map')
 
 describe('map()', () => {
-  let myMap: Map<string, number> = new Map([ [ 'foo', 1 ], [ 'bar', 2 ], [ 'baz', 3 ] ])
+  let myMap = new Map([ [ 'foo', 1 ], [ 'bar', 2 ], [ 'baz', 3 ] ])
   it('Should call cb once for every key/value in Map', () => {
     const cb = sinon.spy()
     const cb2 = sinon.spy()
@@ -16,10 +16,9 @@ describe('map()', () => {
     expect(cb2.callCount).to.equal(2)
   })
 
-  it.only('Should change value of map', () => {
-    const result = map((v: number) => v + 1, myMap)
+  it('Should change value of map', () => {
+    const result = map(v => v + 1, myMap)
     const expected = new Map([ [ 'foo', 2 ], [ 'bar', 3 ], [ 'baz', 4 ] ])
-    console.log({ result, expected })
-    expect(result).to.equal(expected)
+    expect(result).to.eql(expected)
   })
 })
