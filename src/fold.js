4f7eeafcf117115768805f7dc317aceac0bdfe88
Tyler Haas <haasdevelopment@gmail.com>
2017-08-13 18:04:08 -0500

    fold optionally sets initialValue

--- src/fold.js
+++ src/fold.js
@@ -1,12 +1,24 @@
 // @flow
 
 function fold(
-  cb: (accumulator: mixed, currentValue: mixed, nextValue?: mixed) => mixed,
-  inputMap: Map<*, *>
+  cb: (accumulator: mixed, currentValue: mixed, currentKey: mixed) => mixed,
+  inputMap: Map<*, *>,
+  initialValue?: mixed
 ): mixed {
   if (typeof cb !== 'function') throw new Error(
       `Expected 1st argument in call to filter to be a function but received ${typeof cb}`
     )
+
+  let result
+  if (initialValue) result = initialValue
+
+  /*
+  for (const [ k, v ] of inputMap) {
+    result = cb(result, v, k)
+  }
+  */
+
+  return result
 }
 
 module.exports = fold
--- test/fold.test.js
+++ test/fold.test.js
@@ -2,6 +2,7 @@
 
 const { expect } = require('chai')
 const { describe, it } = require('mocha')
+const sinon = require('sinon')
 
 const fold = require('../lib/fold')
 
@@ -15,5 +16,11 @@ describe('fold()', () => {
     expect(() => fold(2, new Map())).to.throw()
     expect(() => fold([ 1, 2, 3 ], new Map())).to.throw()
     expect(() => fold({ foo: 1, bar: 2, baz: 'quix' }, new Map())).to.throw()
+  })
+
+  it('Should initialize result if initalValue', () => {
+    const cb = sinon.spy()
+    const result = fold(cb, new Map(), 5)
+    expect(result).to.equal(5)
   })
 })
