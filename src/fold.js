e2bfeb909b3b70545106ba0b877b32fbf46845fc
Tyler Haas <haasdevelopment@gmail.com>
2017-08-13 22:35:41 -0500

    set initial value when none is passed in

--- src/fold.js
+++ src/fold.js
@@ -1,16 +1,48 @@
 // @flow
 
-function fold(
-  cb: (accumulator: mixed, currentValue: mixed, currentKey: mixed) => mixed,
+const { curry } = require('ramda')
+
+function uncurriedFold(
+  cb: (accumulator: mixed, currentValue: mixed, currentKey?: mixed) => mixed,
   inputMap: Map<*, *>,
-  initialValue?: mixed
+  initialValue?: mixed = undefined
 ): mixed {
   if (typeof cb !== 'function') throw new Error(
       `Expected 1st argument in call to filter to be a function but received ${typeof cb}`
     )
 
-  let result
-  if (initialValue) result = initialValue
+  if (!initialValue) {
+    inputMap.forEach((v, k, m) => {
+      const vType = typeof v
+      if (![ ...m ].every(v => typeof v[1] === vType)) throw new Error('all types in Map must be the same')
+      switch (vType) {
+        case 'number':
+          initialValue = 0
+          break
+
+        case 'function':
+          initialValue = () => {}
+          break
+
+        case 'object':
+          initialValue = Array.isArray(v) ? (v === null ? null : []) : {}
+          break
+
+        case 'string':
+          initialValue = ''
+          break
+
+        case 'boolean':
+          initialValue = false
+          break
+
+        default:
+          break
+      }
+    })
+  }
+
+  let result = initialValue
 
   for (const [ k, v ] of inputMap) {
     result = cb(result, v, k)
@@ -18,5 +50,7 @@ function fold(
 
   return result
 }
+
+const fold = curry(uncurriedFold)
 
 module.exports = fold
--- test/fold.test.js
+++ test/fold.test.js
@@ -26,8 +26,38 @@ describe('fold()', () => {
 
   it('Should call cb for each key/value pair', () => {
     const cb = sinon.spy()
-    const myMap = new Map([ [ 'foo', 5 ], [ { bar: 5 }, 'quix' ], [ () => {}, 7 ] ])
+    const myMap = new Map([ [ 'foo', 5 ], [ { bar: 5 }, 3 ], [ () => {}, 7 ] ])
     fold(cb, myMap)
     expect(cb.callCount).to.equal(3)
+  })
+
+  it('Should return correct result when working with numbers', () => {
+    const myMap = new Map([ [ 'foo', 5 ], [ 'bar', 3 ], [ 'baz', 10 ] ])
+    const result = fold((a, v) => (a += v), myMap, 5)
+    expect(result).to.equal(23)
+  })
+
+  it('Should return correct result when working with an object', () => {
+    const myMap = new Map([ [ 'foo', 5 ], [ 'bar', 3 ], [ 'baz', 10 ] ])
+    const cb = (a, v, k) => {
+      a[k] = v
+      return a
+    }
+    const result = fold(cb, myMap, {})
+    expect(result).to.eql({ foo: 5, bar: 3, baz: 10 })
+  })
+
+  it('Should return function when partially applied', () => {
+    expect(fold(sinon.spy())).to.be.an.instanceof(Function)
+    const myMap = new Map([ [ 'foo', 5 ], [ 'bar', 3 ], [ 'baz', 10 ] ])
+    const result = fold((a, v) => (a += v), myMap)
+    expect(result).to.equal(18)
+  })
+
+  it('Should set initialValue to empty version of passed in datatype', () => {
+    const myMap = new Map([ [ 'foo', 5 ], [ 'bar', 3 ], [ 'baz', 10 ] ])
+    expect(fold((a, v) => (a += v), myMap)).to.equal(18)
+    const myMap2 = new Map([ [ 'foo', '5' ], [ 'bar', '3' ], [ 'baz', '10' ] ])
+    expect(fold((a, v) => (a += v), myMap2)).to.equal('5310')
   })
 })
