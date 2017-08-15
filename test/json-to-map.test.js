d216449a4408f5a6f6f448602975e24bade224d6
Tyler Haas <haasdevelopment@gmail.com>
2017-08-15 10:56:55 -0500

    remove unnecessary test

--- test/json-to-map.test.js
+++ test/json-to-map.test.js
@@ -4,7 +4,7 @@ const { expect } = require('chai')
 const sinon = require('sinon')
 
 const jsonToMap = require('../lib/json-to-map')
-// const objToMap = require('../lib/object-to-map')
+const objToMap = require('../lib/object-to-map')
 
 const { describe, it } = global
 describe('jsonToMap()', () => {
@@ -19,16 +19,6 @@ describe('jsonToMap()', () => {
     expect(JSON.parse.calledWith(json)).to.equal(true)
     JSON.parse.restore()
   })
-
-  /** @todo figure out how to use spies in this way */
-  /*
-  it('Should call objToMap', () => {
-    const spy = sinon.spy(objToMap)
-    const json = '{"a":1,"b":2}'
-    jsonToMap(json)
-    expect(spy.calledWith(JSON.parse(json))).to.equal(true)
-  })
-  */
 
   it('Should return a Map', () => {
     const result = jsonToMap('{"a":1,"b":2}')
