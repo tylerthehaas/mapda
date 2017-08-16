9b8a767ce0684bc6fcac9ac201a9deea8b769e87
Tyler Haas <haasdevelopment@gmail.com>
2017-08-15 20:40:14 -0500

    make initialValue required

--- src/reduce.js
+++ src/reduce.js
@@ -5,44 +5,12 @@ const { curry } = require('ramda')
 /** @todo change this to make initial value required */
 function uncurriedReduce(
   cb: (accumulator: mixed, currentValue: mixed, currentKey?: mixed) => mixed,
-  inputMap: Map<*, *>,
-  initialValue?: mixed = undefined
+  initialValue: mixed = undefined,
+  inputMap: Map<*, *>
 ): mixed {
   if (typeof cb !== 'function') throw new Error(
       `Expected 1st argument in call to filter to be a function but received ${typeof cb}`
     )
-
-  if (!initialValue) {
-    const mapArr = [ ...inputMap ]
-    const [ [ , first ] ] = mapArr
-    const vType = typeof first
-    const isConsistentType = mapArr.every((v: any) => typeof v[1] === vType)
-    if (!isConsistentType) throw new Error('all types in Map must be the same')
-    switch (vType) {
-      case 'number':
-        initialValue = 0
-        break
-
-      case 'function':
-        initialValue = () => {}
-        break
-
-      case 'object':
-        initialValue = Array.isArray(first) ? (first === null ? null : []) : {}
-        break
-
-      case 'string':
-        initialValue = ''
-        break
-
-      case 'boolean':
-        initialValue = false
-        break
-
-      default:
-        break
-    }
-  }
 
   let result = initialValue
 
